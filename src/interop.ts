e47c179f7981591aa5174d8fa643aa357de63b30
CocoShun <40754930+CocoShun@users.noreply.github.com>
2026-07-31 14:22:41 -0500

    Update interop.ts

--- src/interop.ts
+++ src/interop.ts
@@ -7,6 +7,7 @@ export const resume = callable<[pid: number], boolean>("resume");
 // export const kill = callable<[pid: number], boolean>("kill");
 export const pid_from_appid = callable<[appid: number], number>("pid_from_appid");
 export const appid_from_pid = callable<[pid: number], number>("appid_from_pid");
+export const set_focused_pid = callable<[pid: number], void>("set_focused_pid");
 export const load_settings = callable<[], any>("load_settings");
 export const save_setting = callable<[key: string, value: any], void>("save_setting");
 export const add_no_auto_pause_set = callable<[appid: number], void>("add_no_auto_pause_set");
